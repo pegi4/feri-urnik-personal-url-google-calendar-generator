928d3c66a3590863546952e651d4eff9e39ac7e5
Vito Senic <67893442+pegi4@users.noreply.github.com>
2024-10-03 16:46:37 +0200

    passing params into url

--- src/app/api/calendar/route.js
+++ src/app/api/calendar/route.js
@@ -1,86 +1,90 @@
 import { NextResponse } from 'next/server';
 import puppeteer from 'puppeteer';
 
-const predmetSkupina = {
-    "UVOD V PLATFORMNO ODVISEN RAZVOJ APLIKACIJ": "1",
-    "PODATKOVNE BAZE I": "1",
-    "RAČUNALNIŠKA OMREŽJA": "3",
-    "UPORABNIŠKI VMESNIKI": "4",
-    "DISKRETNA MATEMATIKA": null,
-    "OSNOVE STATISTIKE": "3"
-  };
+// Funkcija za pretvorbo query parametrov v predmet-skupina objekt
+function parseSubjects(subjectsParam) {
+  const subjectArray = subjectsParam.split(';');
+  const predmetSkupina = {};
 
-  function dodajVrstoVSummary(eventLines, vrsta) {
-    return eventLines.map(line => {
+  subjectArray.forEach((item) => {
+    const [predmet, skupina] = item.split(',');
+    predmetSkupina[predmet] = skupina === 'null' ? null : skupina;
+  });
+
+  return predmetSkupina;
+}
+
+function dodajVrstoVSummary(eventLines, vrsta) {
+  return eventLines.map(line => {
+    if (line.startsWith("SUMMARY:")) {
+      const predmet = line.split("SUMMARY:")[1].trim();
+      return `SUMMARY:${predmet} (${vrsta})\n`;
+    }
+    return line;
+  });
+}
+
+function filtrirajIcs(data, predmetSkupina) {
+  const lines = data.split('\n');
+  let filtriraneVrstice = [];
+  let isEvent = false;
+  let currentEvent = [];
+  let currentPredmet = null;
+  let vrstaDogodka = null;
+
+  for (let line of lines) {
+    if (line.startsWith("BEGIN:VEVENT")) {
+      isEvent = true;
+      currentEvent = [];
+      currentPredmet = null;
+      vrstaDogodka = null;
+    }
+
+    if (isEvent) {
+      currentEvent.push(line);
+
       if (line.startsWith("SUMMARY:")) {
-        const predmet = line.split("SUMMARY:")[1].trim();
-        return `SUMMARY:${predmet} (${vrsta})\n`;
+        currentPredmet = line.split("SUMMARY:")[1].trim();
       }
-      return line;
-    });
-  }
 
-  function filtrirajIcs(data, predmetSkupina) {
-    const lines = data.split('\n');
-    let filtriraneVrstice = [];
-    let isEvent = false;
-    let currentEvent = [];
-    let currentPredmet = null;
-    let vrstaDogodka = null;
-  
-    for (let line of lines) {
-      if (line.startsWith("BEGIN:VEVENT")) {
-        isEvent = true;
-        currentEvent = [];
-        currentPredmet = null;
-        vrstaDogodka = null;
-      }
-  
-      if (isEvent) {
-        currentEvent.push(line);
-  
-        if (line.startsWith("SUMMARY:")) {
-          currentPredmet = line.split("SUMMARY:")[1].trim();
-        }
-  
-        if (line.startsWith("DESCRIPTION:")) {
-          const description = line.split("DESCRIPTION:")[1].trim();
-          if (description.includes("PR")) {
-            vrstaDogodka = "Predavanje";
-          } else if (description.includes("SV")) {
-            vrstaDogodka = "Seminarske vaje";
-          } else if (description.includes("RV")) {
-            vrstaDogodka = "Računalniške vaje";
-          }
+      if (line.startsWith("DESCRIPTION:")) {
+        const description = line.split("DESCRIPTION:")[1].trim();
+        if (description.includes("PR")) {
+          vrstaDogodka = "Predavanje";
+        } else if (description.includes("SV")) {
+          vrstaDogodka = "Seminarske vaje";
+        } else if (description.includes("RV")) {
+          vrstaDogodka = "Računalniške vaje";
         }
       }
-  
-      if (line.startsWith("END:VEVENT")) {
-        isEvent = false;
-        let eventDescription = currentEvent.join('');
-  
-        if (currentPredmet) {
-          if (eventDescription.includes("RIT 2 VS")) {
-            if (eventDescription.includes("RV")) {
-              const match = eventDescription.match(/RV (\d+)/);
-              if (match) {
-                const skupina = match[1];
-                if (predmetSkupina[currentPredmet] === skupina) {
-                  currentEvent = dodajVrstoVSummary(currentEvent, vrstaDogodka);
-                  filtriraneVrstice.push(...currentEvent);
-                }
+    }
+
+    if (line.startsWith("END:VEVENT")) {
+      isEvent = false;
+      let eventDescription = currentEvent.join('');
+
+      if (currentPredmet) {
+        if (eventDescription.includes("RIT 2 VS")) {
+          if (eventDescription.includes("RV")) {
+            const match = eventDescription.match(/RV (\d+)/);
+            if (match) {
+              const skupina = match[1];
+              if (predmetSkupina[currentPredmet] === skupina) {
+                currentEvent = dodajVrstoVSummary(currentEvent, vrstaDogodka);
+                filtriraneVrstice.push(...currentEvent);
               }
-            } else {
-              currentEvent = dodajVrstoVSummary(currentEvent, vrstaDogodka);
-              filtriraneVrstice.push(...currentEvent);
             }
+          } else {
+            currentEvent = dodajVrstoVSummary(currentEvent, vrstaDogodka);
+            filtriraneVrstice.push(...currentEvent);
           }
         }
       }
     }
-  
-    return `BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:WISE TIMETABLE\n` + filtriraneVrstice.join('\n') + `\nEND:VCALENDAR`;
   }
+
+  return `BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:WISE TIMETABLE\n` + filtriraneVrstice.join('\n') + `\nEND:VCALENDAR`;
+}
 
 async function getTitles(page, filterId) {
   const subjectFilter = filterId.split(';', 4).pop();
@@ -153,30 +157,34 @@ async function fetchCalendar(filterId) {
   }
 }
 
-// Znotraj te funkcije obdelamo API request
+// API handler funkcija
 export async function GET(request) {
-    const { searchParams } = new URL(request.url);
-    const filterId = searchParams.get('filterId');
-  
-    if (!filterId) {
-      return NextResponse.json({ error: 'filterId is required' }, { status: 400 });
-    }
-  
-    try {
-      // Pridobi podatke iz WISE Timetable preko puppeteerja
-      const data = await fetchCalendar(filterId);
-  
-      // Filtriraj podatke s pomočjo predmeta in skupin
-      const filtriraniPodatki = filtrirajIcs(data, predmetSkupina);
-  
-      // Vrni filtrirane podatke kot odgovor
-      return new NextResponse(filtriraniPodatki, {
-        headers: {
-          'content-type': 'text/plain; charset=utf-8',
-        }
-      });
-    } catch (e) {
-      console.error(e);
-      return NextResponse.json({ error: 'Could not fetch calendar' }, { status: 500 });
-    }
+  const { searchParams } = new URL(request.url);
+  const filterId = searchParams.get('filterId');
+  const subjectsParam = searchParams.get('subjects');
+
+  if (!filterId || !subjectsParam) {
+    return NextResponse.json({ error: 'filterId and subjects are required' }, { status: 400 });
   }
+
+  try {
+    // Pridobi predmet-skupina podatke iz URL-ja
+    const predmetSkupina = parseSubjects(subjectsParam);
+
+    // Pridobi podatke iz WISE Timetable preko puppeteerja
+    const data = await fetchCalendar(filterId); // Tukaj uporabi `filterId`, ne `subjectsParam`
+
+    // Filtriraj podatke s pomočjo predmeta in skupin
+    const filtriraniPodatki = filtrirajIcs(data, predmetSkupina);
+
+    // Vrni filtrirane podatke kot odgovor
+    return new NextResponse(filtriraniPodatki, {
+      headers: {
+        'content-type': 'text/plain; charset=utf-8',
+      }
+    });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: 'Could not fetch calendar' }, { status: 500 });
+  }
+}
